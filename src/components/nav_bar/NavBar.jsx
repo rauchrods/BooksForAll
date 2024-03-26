05443b7f91d35e20445ff44a7bb5d561e39592ca
Rauch Rodrigues <rauch.rodrigues@koseapp.com>
2024-03-26 21:11:50 +0530

    updated navbar

--- src/components/nav_bar/NavBar.jsx
+++ src/components/nav_bar/NavBar.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./NavBar.module.scss";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { IoIosMenu } from "react-icons/io";
+import { MdMenuOpen } from "react-icons/md";
 
 function NavBar() {
+  const [isMenuOpened, setIsMenuOpened] = useState(false);
+
   const currentUser = useSelector((state) => state.currentUser);
+
+  function menuOpenHandler() {
+    setIsMenuOpened((currState) => !currState);
+  }
 
   return (
     <nav className={styles.navbar}>
@@ -30,6 +38,31 @@ function NavBar() {
           </Link>
         )}
       </div>
+      <div className={styles.ham_menu} onClick={menuOpenHandler}>
+        {isMenuOpened ? <MdMenuOpen /> : <IoIosMenu />}
+      </div>
+
+      {isMenuOpened && (
+        <div className={styles.ham_menu_display}>
+          {currentUser && (
+            <Link to={"/yourbooks"}>
+              <span onClick={menuOpenHandler}>Your Books</span>
+            </Link>
+          )}
+          <Link to={"/about"}>
+            <span onClick={menuOpenHandler}>About</span>
+          </Link>
+          {currentUser ? (
+            <Link to={"/profile"}>
+              <span onClick={menuOpenHandler}>{currentUser.userName}</span>
+            </Link>
+          ) : (
+            <Link to={"/signup"}>
+              <span onClick={menuOpenHandler}>SignIn</span>
+            </Link>
+          )}
+        </div>
+      )}
     </nav>
   );
 }
